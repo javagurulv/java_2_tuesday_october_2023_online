d4b56378bbc3d8bcb239e2b64a9abd2a2d127161
javagurulv <javagurulv@gmail.com>
2023-10-03 18:21:23 +0300

    travel insurance calculator

---
+++ travel_insurance_calculator/step_3/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -0,0 +1,20 @@
+package lv.javaguru.travel.insurance.core;
+
+import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+
+    @Override
+    public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        response.setPersonFirstName(request.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
+        return response;
+    }
+
+}
---
+++ travel_insurance_calculator/step_6/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidator.java
@@ -0,0 +1,26 @@
+package lv.javaguru.travel.insurance.core;
+
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+class TravelCalculatePremiumRequestValidator {
+
+    public List<ValidationError> validate(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = new ArrayList<>();
+        validatePersonFirstName(request).ifPresent(errors::add);
+        return errors;
+    }
+
+    private Optional<ValidationError> validatePersonFirstName(TravelCalculatePremiumRequest request) {
+        return (request.getPersonFirstName() == null || request.getPersonLastName().isEmpty())
+                ? Optional.of(new ValidationError("personFirstName", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+}
