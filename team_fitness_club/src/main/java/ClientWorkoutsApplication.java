69f5c6ca6fa53b683eaafc2378f38eee53ae134d
Natalia <natukssa@gmail.com>
2023-10-08 13:29:15 +0300

    Client Workouts change to have choice

--- team_fitness_club/src/main/java/ClientWorkoutsApplication.java
+++ team_fitness_club/src/main/java/ClientWorkoutsApplication.java
@@ -32,8 +32,25 @@ public class ClientWorkoutsApplication {
                             toUpperCase().concat(clientLastName.substring(1)) : clientLastName;
                     System.out.println("Enter client personal code: ");
                     String clientPersonalCode = scanner.nextLine();
-                    System.out.println("Enter client workout: ");
-                    Workouts clientWorkout = Workouts.valueOf(scanner.nextLine());
+                    System.out.println("Choose client workout.");
+                    System.out.println("1. GYM");
+                    System.out.println("2. Swimming Pool");
+                    System.out.println("3. Group Classes");
+                    int userChoiceWorkout = Integer.parseInt(scanner.nextLine());
+                    Workouts clientWorkout = null;
+                    switch (userChoiceWorkout) {
+                        case 1: {
+                            clientWorkout = Workouts.GYM;
+                            break;
+                        }
+                        case 2: {
+                            clientWorkout = Workouts.SWIMMING_POOL;
+                            break;
+                        }
+                        case 3: {
+                            clientWorkout = Workouts.GROUP_CLASSES;
+                        }
+                    }
                     Client client = new Client(clientFirstName, clientLastName, clientPersonalCode, clientWorkout);
                     client.setId(id);
                     clients.add(client);
@@ -69,16 +86,42 @@ public class ClientWorkoutsApplication {
                     System.out.println("Enter client personal code: ");
                     String clientPersonalCode = scanner.nextLine();
                     Client changeClientWorkout = new Client(clientFirstName, clientLastName, clientPersonalCode);
-                    System.out.println("Enter client new workout: ");
-                    Workouts newWorkout = Workouts.valueOf(scanner.nextLine());
-                    for (Client client : clients) {
-                        if (client.equals(changeClientWorkout)) {
-                            client.setWorkouts(newWorkout);
+                    System.out.println("Choose new workout.");
+                    System.out.println("1. GYM");
+                    System.out.println("2. Swimming Pool");
+                    System.out.println("3. Group Classes");
+                    int userChoiceWorkout = Integer.parseInt(scanner.nextLine());
+                    switch (userChoiceWorkout){
+                        case 1: {
+                            for (Client client : clients) {
+                                if (client.equals(changeClientWorkout)) {
+                                    client.setWorkouts(Workouts.GYM);
+                                }
+                            }
+                            System.out.println("Client workout has changed to GYM.");
+                            break;
+                        }
+                        case 2: {
+                            for (Client client : clients) {
+                                if (client.equals(changeClientWorkout)) {
+                                    client.setWorkouts(Workouts.SWIMMING_POOL);
+                                }
+                            }
+                            System.out.println("Client workout has changed to Swimming Pool.");
+                            break;
+                        }
+                        case 3: {
+                            for (Client client : clients) {
+                                if (client.equals(changeClientWorkout)) {
+                                    client.setWorkouts(Workouts.GROUP_CLASSES);
+                                }
+                            }
+                            System.out.println("Client workout has changed to Group Classes.");
+                            break;
                         }
                     }
-                    System.out.println("Client workout was changed.");
                     break;
-                }
+                    }
                 case 5: {
                     System.out.println("All the best");
                     System.exit(0);
