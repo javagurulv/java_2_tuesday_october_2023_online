1bcc92206935e28302d764a7e6ae30134ab274c6
AndrejsCekalins <batarejka2006@gmail.com>
2023-10-11 13:41:04 +0300

    student_andrejs_cekalins_travel_insurance_calculator step_2_fillResponseFields and step_4_agreementPrice

--- student_andrejs_cekalins_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_andrejs_cekalins_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -4,12 +4,22 @@ import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
-
+private  DateTimeService dateTimeService;
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
-        return new TravelCalculatePremiumResponse();
-    }
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        response.setPersonFirstName(request.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
 
+        long differenceDays = dateTimeService.getDifferenceDays(request.getAgreementDateFrom(),request.getAgreementDateTo()); {
+            response.setAgreementPrice(new BigDecimal(differenceDays));
+        }
+        return response;
+    }
 }
