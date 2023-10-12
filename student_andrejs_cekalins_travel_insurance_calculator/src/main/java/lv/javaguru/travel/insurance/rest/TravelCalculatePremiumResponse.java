84e3a92045da7bba3d4514018fe29b4240d53d9e
AndrejsCekalins <batarejka2006@gmail.com>
2023-10-12 17:05:20 +0300

    student_andrejs_cekalins_travel_insurance_calculator step_5_LombokInUse

--- student_andrejs_cekalins_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_andrejs_cekalins_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -1,8 +1,17 @@
 package lv.javaguru.travel.insurance.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelCalculatePremiumResponse {
 
     private String personFirstName;
@@ -10,44 +19,4 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
-
-    public TravelCalculatePremiumResponse() {}
-
-    public String getPersonFirstName() {
-        return personFirstName;
-    }
-
-    public void setPersonFirstName(String personFirstName) {
-        this.personFirstName = personFirstName;
-    }
-
-    public String getPersonLastName() {
-        return personLastName;
-    }
-
-    public void setPersonLastName(String personLastName) {
-        this.personLastName = personLastName;
-    }
-
-    public Date getAgreementDateFrom() {
-        return agreementDateFrom;
-    }
-
-    public void setAgreementDateFrom(Date agreementDateFrom) {
-        this.agreementDateFrom = agreementDateFrom;
-    }
-
-    public Date getAgreementDateTo() {
-        return agreementDateTo;
-    }
-
-    public void setAgreementDateTo(Date agreementDateTo) {
-        this.agreementDateTo = agreementDateTo;
-    }
-    public void setAgreementPrice(BigDecimal agreementPrice) {
-        this.agreementPrice = agreementPrice;
-    }
-    public BigDecimal getAgreementPrice() {
-        return agreementPrice;
-    }
 }
