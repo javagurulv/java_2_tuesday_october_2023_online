43958f858897a1629af9090cda29fac006a307a4
javagurulv <javagurulv@gmail.com>
2023-10-13 18:08:10 +0300

    steps 11 - 20

---
+++ teacher/travel_insurance_calculator/step_18/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -0,0 +1,54 @@
+package lv.javaguru.travel.insurance.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TravelCalculatePremiumControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Autowired private JsonFileReader jsonFileReader;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void simpleRestControllerTest() throws Exception {
+        executeAndCompare(
+                "rest/TravelCalculatePremiumRequest.json",
+                "rest/TravelCalculatePremiumResponse.json"
+        );
+    }
+
+    private void executeAndCompare(String jsonRequestFilePath,
+                                   String jsonResponseFilePath) throws Exception {
+        String jsonRequest = jsonFileReader.readJsonFromFile(jsonRequestFilePath);
+
+        MvcResult result = mockMvc.perform(post("/insurance/travel/")
+                        .content(jsonRequest)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBodyContent = result.getResponse().getContentAsString();
+
+        String jsonResponse = jsonFileReader.readJsonFromFile(jsonResponseFilePath);
+
+        assertEquals(mapper.readTree(responseBodyContent), mapper.readTree(jsonResponse));
+    }
+
+}
