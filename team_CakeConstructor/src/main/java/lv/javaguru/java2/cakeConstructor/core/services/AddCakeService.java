b297ec4955cd87d94147b5a077393512097eaab4
IchiuRain <ichiu.rain011@gmail.com>
2023-10-18 11:44:34 +0300

    team_cakeConstructor_2

---
+++ team_CakeConstructor/src/main/java/lv/javaguru/java2/cakeConstructor/core/services/AddCakeService.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java2.cakeConstructor.core.services;
+
+import lv.javaguru.java2.cakeConstructor.core.database.DataBase;
+import lv.javaguru.java2.cakeConstructor.core.domain.Cake;
+import lv.javaguru.java2.cakeConstructor.core.domain.ListOfIngridients;
+import lv.javaguru.java2.cakeConstructor.core.request.AddCakeRequest;
+import lv.javaguru.java2.cakeConstructor.core.responses.AddCakeResponse;
+
+public class AddCakeService {
+
+    private DataBase dataBase;
+    private ListOfIngridients list = new ListOfIngridients();
+    public AddCakeService (DataBase dataBase){
+        this.dataBase=dataBase;
+    }
+
+    public AddCakeResponse execute(AddCakeRequest request){
+        Cake cake = list.createCake(request.getClientId());
+        dataBase.add(cake);
+        return new AddCakeResponse(cake);
+    }
+}
