cfd17faa4f6e368a492d7655573001078c920b22
Оксана Tarasova <oksana_sineok@mail.ru>
2023-10-19 19:40:33 +0300

    step_12_refactor_serviceImplTest

--- student_oksana_tarasova_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_oksana_tarasova_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -5,9 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lv.javaguru.travel.insurance.core.DateTimeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -17,7 +15,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+
 public class TravelCalculatePremiumResponse extends CoreResponse {
 
     private String personFirstName;
@@ -25,10 +23,11 @@ public class TravelCalculatePremiumResponse extends CoreResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
-    private List<ValidationError> errors;
 
-    @Autowired private CoreResponse coreResponse;
-    @Autowired private DateTimeService dateTimeService;
+
+    public TravelCalculatePremiumResponse(List<ValidationError> errors) {
+        super(errors);
+    }
 
 
 }
--- student_oksana_tarasova_travel_insurance_calculator/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_oksana_tarasova_travel_insurance_calculator/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,53 +1,52 @@
 package lv.javaguru.travel.insurance.core;
 
-import lv.javaguru.travel.insurance.dto.CoreResponse;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import lv.javaguru.travel.insurance.dto.ValidationError;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TravelCalculatePremiumServiceImplTest {
-/*
 
     TravelCalculatePremiumServiceImpl serviceImpl = new TravelCalculatePremiumServiceImpl();
     TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
     TravelCalculatePremiumRequestValidator validator = new TravelCalculatePremiumRequestValidator();
-    CoreResponse coreResponse = new CoreResponse();
-    List<ValidationError> errors = new ArrayList<>();
 
-    @BeforeEach
-    public void setUpRequest() throws ParseException {
+
+    @Test
+    public void getFirstNameResponse() throws ParseException {
         Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
         Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
                 "Ivan", "Ivanov", dateFrom,
                 dateTo);
+        validator.validate(request);
         response = serviceImpl.calculatePremium(request);
-    }
-
-
-    @Test
-    public void getFirstNameResponse() {
         String firstNameActual = response.getPersonFirstName();
-
         assertEquals("Ivan", firstNameActual);
 
 
+
     }
 
     @Test
-    public void getLastNameResponse() {
+    public void getLastNameResponse() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan", "Ivanov", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
 
         String lastNameActual = response.getPersonLastName();
         assertEquals("Ivanov", lastNameActual);
@@ -55,17 +54,127 @@ class TravelCalculatePremiumServiceImplTest {
 
     @Test
     public void getDateFrom() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan", "Ivanov", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
         Date dateFrom1 = response.getAgreementDateFrom();
         assertEquals(new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000"), dateFrom1);
     }
 
     @Test
     public void getDateTo() throws ParseException {
-
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan", "Ivanov", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
         Date dateTo1 = response.getAgreementDateTo();
         assertEquals(new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000"), dateTo1);
     }
 
-*/
+    @Test
+    public void getErrorFirstName() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                null, "Ivanov", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        assertThat(response.getErrors()).contains(new ValidationError("personFirstName", "Must not be empty!"));
+    }
+
+    @Test
+    public void getErrorLastName() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan", "", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        assertThat(response.getErrors()).contains(new ValidationError("personLastName", "Must not be empty!"));
+    }
+
+    @Test
+    public void getErrorsFirstAndLastName() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "", "", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        assertThat(response.getErrors()).hasSize(2);
+        assertThat(response.getErrors()).contains(new ValidationError("personFirstName", "Must not be empty!"));
+        assertThat(response.getErrors()).contains(new ValidationError("personLastName", "Must not be empty!"));
+    }
+
+    @Test
+    public void getErrorsFirstAndLastNameAndDateFrom() throws ParseException {
+        Date dateFrom = null;
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "", "", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        assertThat(response.getErrors()).hasSize(3);
+        assertThat(response.getErrors()).contains(new ValidationError("personFirstName", "Must not be empty!"));
+        assertThat(response.getErrors()).contains(new ValidationError("personLastName", "Must not be empty!"));
+        assertThat(response.getErrors()).contains(new ValidationError("agreementDateFrom", "Must not be empty!"));
+    }
+    @Test
+    public void getErrorsFirstAndLastNameAndDateFromAndDateTo() throws ParseException {
+        Date dateFrom = null;
+        Date dateTo = null;
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "", "", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        assertThat(response.getErrors()).hasSize(4);
+        assertThat(response.getErrors()).contains(new ValidationError("personFirstName", "Must not be empty!"));
+        assertThat(response.getErrors()).contains(new ValidationError("personLastName", "Must not be empty!"));
+        assertThat(response.getErrors()).contains(new ValidationError("agreementDateFrom", "Must not be empty!"));
+        assertThat(response.getErrors()).contains(new ValidationError("agreementDateTo", "Must not be empty!"));
+    }
+
+    @Test
+    public void getErrorDateFromIsLessDateTo() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan", "Ivanov", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        assertThat(response.getErrors()).hasSize(1);
+        assertThat(response.getErrors()).contains(new ValidationError("agreementDateFrom", "Must be less than the agreementDateTo!"));
+    }
+
+    @Test
+
+    public void getAgreementPrice() throws ParseException {
+        Date dateFrom = new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2000");
+        Date dateTo = new SimpleDateFormat("dd/MM/yyyy").parse("16/05/2000");
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan", "Ivanov", dateFrom,
+                dateTo);
+        validator.validate(request);
+        response = serviceImpl.calculatePremium(request);
+        BigDecimal betweenDays = response.getAgreementPrice();
+        assertEquals(betweenDays, new BigDecimal(15));
+
+
+
+    }
 
 }
+
