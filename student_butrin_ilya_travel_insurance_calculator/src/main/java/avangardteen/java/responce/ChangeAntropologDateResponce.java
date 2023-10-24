9cdb4af6874de9cbe6b5eb45114a2440cb477108
westor0007 <dfglkj@ya.ru>
2023-10-24 22:38:56 +0300

    project

---
+++ student_butrin_ilya_travel_insurance_calculator/src/main/java/avangardteen/java/responce/ChangeAntropologDateResponce.java
@@ -0,0 +1,16 @@
+package avangardteen.java.responce;
+
+import avangardteen.java.CoreError;
+import avangardteen.java.CoreResponce;
+import avangardteen.java.service.valigation.ChangeAntropologDateValigation;
+
+import java.util.List;
+
+public class ChangeAntropologDateResponce extends CoreResponce {
+    public ChangeAntropologDateResponce(List<CoreError> errorList) {
+        super(errorList);
+    }
+
+    public ChangeAntropologDateResponce() {
+    }
+}
