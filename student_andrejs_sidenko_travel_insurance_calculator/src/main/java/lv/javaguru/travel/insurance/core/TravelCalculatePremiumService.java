777d6218fc65e1208aab920d14361a16cc86dc94
Anetobiz <anetobiz@gmail.com>
2023-10-26 18:11:15 +0300

    Travel insurance calculator step_10 commit.

--- student_andrejs_sidenko_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumService.java
+++ student_andrejs_sidenko_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumService.java
@@ -1,7 +1,7 @@
 package lv.javaguru.travel.insurance.core;
 
-import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
-import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 
 public interface TravelCalculatePremiumService {
 
