c4df2d6048a958d2cd2aadc08958063652c9991a
IchiuRain <ichiu.rain011@gmail.com>
2023-10-28 19:11:04 +0300

    travel

--- team_CakeConstructor/src/main/java/lv/javaguru/java2/cakeConstructor/core/cake/services/SearchUserRequestValidation.java
+++ team_CakeConstructor/src/main/java/lv/javaguru/java2/cakeConstructor/core/cake/services/SearchUserRequestValidation.java
@@ -1,2 +1,31 @@
-package lv.javaguru.java2.cakeConstructor.core.cake.services;public class SearchUserRequestValidation {
+package lv.javaguru.java2.cakeConstructor.core.cake.services;
+
+import lv.javaguru.java2.cakeConstructor.core.cake.request.SearchUserRequest;
+import lv.javaguru.java2.cakeConstructor.core.cake.responses.CoreError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchUserRequestValidation {
+
+    public List<CoreError> validate(SearchUserRequest request) {
+        List<CoreError> errors = new ArrayList<>();
+        errors.addAll(validateSearchFields(request));
+        return errors;
+    }
+
+    private List<CoreError> validateSearchFields(SearchUserRequest request) {
+        List<CoreError> errors = new ArrayList<>();
+        if (isEmpty(request.getLogin()) && isEmpty(request.getUserName())) {
+            errors.add(new CoreError("Login", "Must not be empty!"));
+            errors.add(new CoreError("UserName", "Must not be empty!"));
+        }
+        return errors;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
 }
+
