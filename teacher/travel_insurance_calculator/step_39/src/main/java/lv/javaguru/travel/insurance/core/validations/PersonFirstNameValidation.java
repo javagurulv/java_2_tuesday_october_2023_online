ee2c983d28bc6795c7a7a910c2e6bf08bbbe0a80
javagurulv <javagurulv@gmail.com>
2023-11-01 18:55:36 +0200

    step 31 - 40

---
+++ teacher/travel_insurance_calculator/step_39/src/main/java/lv/javaguru/travel/insurance/core/validations/PersonFirstNameValidation.java
@@ -0,0 +1,19 @@
+package lv.javaguru.travel.insurance.core.validations;
+
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+class PersonFirstNameValidation implements TravelRequestValidation {
+
+    @Override
+    public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
+        return (request.getPersonFirstName() == null || request.getPersonFirstName().isEmpty())
+                ? Optional.of(new ValidationError("personFirstName", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+}
---
+++ teacher/travel_insurance_calculator/step_40/src/main/java/lv/javaguru/travel/insurance/core/validations/DateFromLessThenDateToValidation.java
@@ -0,0 +1,32 @@
+package lv.javaguru.travel.insurance.core.validations;
+
+import lv.javaguru.travel.insurance.core.ErrorCodeUtil;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+class DateFromLessThenDateToValidation implements TravelRequestValidation {
+
+    @Autowired private ErrorCodeUtil errorCodeUtil;
+
+    @Override
+    public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
+        Date dateFrom = request.getAgreementDateFrom();
+        Date dateTo = request.getAgreementDateTo();
+        return (dateFrom != null && dateTo != null
+                && (dateFrom.equals(dateTo) || dateFrom.after(dateTo)))
+                ? Optional.of(buildError("ERROR_CODE_5"))
+                : Optional.empty();
+    }
+
+    private ValidationError buildError(String errorCode) {
+        String errorDescription = errorCodeUtil.getErrorDescription(errorCode);
+        return new ValidationError(errorCode, errorDescription);
+    }
+
+}
