e53d4d2d284931d4d0d6722e0b571de6878955d1
DESKTOP-6OUGHTK\DELL <tatjana.grigorcaka@gmail.com>
2023-11-04 18:45:28 +0200

    travel_insurance_calculator_step_26_travelCalculatePremiumRequestExecutionTimeLogger_stopwatch_settings

--- student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -1,5 +1,6 @@
 package lv.javaguru.travel.insurance.rest;
 
+import com.google.common.base.Stopwatch;
 import lv.javaguru.travel.insurance.core.TravelCalculatePremiumService;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
@@ -15,12 +16,20 @@ public class TravelCalculatePremiumController {
 
 	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
 	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
+	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
 
 	@PostMapping(path = "/",
 			consumes = "application/json",
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		TravelCalculatePremiumResponse response = processRequest(request);
+		executionTimeLogger.logExecutionTime(stopwatch);
+		return response;
+	}
+
+	private TravelCalculatePremiumResponse processRequest(TravelCalculatePremiumRequest request) {
 		requestLogger.log(request);
 		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
 		responseLogger.log(response);
