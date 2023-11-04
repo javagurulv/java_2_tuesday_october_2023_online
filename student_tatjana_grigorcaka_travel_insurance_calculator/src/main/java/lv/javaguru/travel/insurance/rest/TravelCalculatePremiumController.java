21a23429e2534d4936f1ef32da02365b2bc41649
DESKTOP-6OUGHTK\DELL <tatjana.grigorcaka@gmail.com>
2023-11-04 18:13:58 +0200

    travel_insurance_calculator_step_25_TravelCalculatePremiumResponseLogger_TravelCalculatePremiumController

--- student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TravelCalculatePremiumController {
 
 	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
+	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
 
 	@PostMapping(path = "/",
@@ -21,7 +22,9 @@ public class TravelCalculatePremiumController {
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
 		requestLogger.log(request);
-		return calculatePremiumService.calculatePremium(request);
+		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+		responseLogger.log(response);
+		return response;
 	}
 
 }
