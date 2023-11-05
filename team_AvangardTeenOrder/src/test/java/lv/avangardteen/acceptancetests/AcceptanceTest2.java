16b117312b84fcb7182c0b3ed8483f948c09e41f
AinarsB <abelinskis@gmail.com>
2023-11-06 00:39:23 +0200

    AvangardT1_AcceptanceTests

---
+++ team_AvangardTeenOrder/src/test/java/lv/avangardteen/acceptancetests/AcceptanceTest2.java
@@ -0,0 +1,63 @@
+package lv.avangardteen.acceptancetests;
+
+import lv.avangardteen.ApplicationContext;
+import lv.avangardteen.core.request.ClientRequest;
+import lv.avangardteen.core.request.DeleteOrderRequest;
+import lv.avangardteen.core.request.ShowOrderRequest;
+import lv.avangardteen.core.responce.ShowOrderResponse;
+import lv.avangardteen.core.service.ClientService;
+import lv.avangardteen.core.service.DeleteOrderService;
+import lv.avangardteen.core.service.ShowOrderService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class AcceptanceTest2 {
+    private ApplicationContext appContext = new ApplicationContext();
+
+    @Test
+    public void shouldDeleteOneOrderData() {
+        ClientRequest request1 = new ClientRequest("Alex", 123456, "Riga", 11,
+                22, 33, 44, 11, 21,
+                 31, 41);
+        getClientService().execute(request1);
+
+        ClientRequest request2 = new ClientRequest("Olga", 54321, "Tallin",
+                11,21, 31, 41, 17,21,
+                31, 41);
+        getClientService().execute(request2);
+
+        DeleteOrderRequest request3 = new DeleteOrderRequest(1L);
+        getDeleteOrderService().execute(request3);
+
+        ShowOrderResponse response = getShowOrderService().execute(new ShowOrderRequest(1L));
+
+        ShowOrderResponse response2 = getShowOrderService().execute(new ShowOrderRequest(2L));
+
+        assertNull(response.getClient());
+
+        assertEquals(response2.getClient().getId(), 2);
+        assertEquals(response2.getClient().getNameSurname(), "Olga");
+        assertEquals(response2.getClient().getPhoneNumber(), 54321);
+        assertEquals(response2.getClient().getUserAddress(), "Tallin");
+        assertEquals(response2.getClient().getUserSizes().getShinLength(), 11);
+        assertEquals(response2.getClient().getUserSizes().getBackHeight(), 21);
+        assertEquals(response2.getClient().getUserSizes().getThighLength(), 31);
+        assertEquals(response2.getClient().getUserSizes().getPelvisWidth(), 41);
+
+    }
+
+    private ClientService getClientService () {
+        return appContext.getBean(ClientService.class);
+    }
+
+    private DeleteOrderService getDeleteOrderService () {
+        return  appContext.getBean(DeleteOrderService.class);
+    }
+
+    private ShowOrderService getShowOrderService () {
+        return appContext.getBean(ShowOrderService.class);
+    }
+
+}
---
+++ team_AvangardTeenOrder/src/test/java/lv/avangardteen/acceptancetests/AcceptanceTest4.java
@@ -0,0 +1,52 @@
+package lv.avangardteen.acceptancetests;
+
+import lv.avangardteen.ApplicationContext;
+import lv.avangardteen.core.request.ClientRequest;
+import lv.avangardteen.core.request.ShowOrderRequest;
+import lv.avangardteen.core.responce.ShowOrderResponse;
+import lv.avangardteen.core.service.ClientService;
+import lv.avangardteen.core.service.ShowOrderService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AcceptanceTest4 {
+    private ApplicationContext appContext = new ApplicationContext();
+
+    @Test
+    public void checkPriceOfDifferentBrakes() {
+        ClientRequest request1 = new ClientRequest("Alex", 123456, "Riga", 11,
+                22, 33, 44, 11, 21,
+                 31, 41);
+        getClientService().execute(request1);
+
+        ClientRequest request2 = new ClientRequest("Alex", 123456, "Riga", 11,
+                22, 33, 44, 11, 21,
+                32, 41);
+        getClientService().execute(request2);
+
+        ClientRequest request3 = new ClientRequest("Alex", 123456, "Riga", 11,
+                22, 33, 44, 11, 21,
+                33, 41);
+        getClientService().execute(request3);
+
+        ShowOrderResponse response4 = getShowOrderService().execute(new ShowOrderRequest(1L));
+        ShowOrderResponse response5 = getShowOrderService().execute(new ShowOrderRequest(2L));
+        ShowOrderResponse response6 = getShowOrderService().execute(new ShowOrderRequest(3L));
+
+        assertEquals(response4.getClient().getId(), 1);
+        assertEquals(response4.getClient().getPriseOrder(), 199700);
+        assertEquals(response5.getClient().getId(), 2);
+        assertEquals(response5.getClient().getPriseOrder(), 201200);
+        assertEquals(response6.getClient().getId(), 3);
+        assertEquals(response6.getClient().getPriseOrder(), 205400);
+    }
+
+    private ClientService getClientService () {
+        return appContext.getBean(ClientService.class);
+    }
+    private ShowOrderService getShowOrderService () {
+        return appContext.getBean(ShowOrderService.class);
+    }
+
+}
