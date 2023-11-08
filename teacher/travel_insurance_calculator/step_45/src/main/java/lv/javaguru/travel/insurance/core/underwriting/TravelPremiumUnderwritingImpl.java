799aa0bdec05118f3e88f932003e5ca0ccd6261b
javagurulv <javagurulv@gmail.com>
2023-11-08 08:49:04 +0200

    step 41 - 50

---
+++ teacher/travel_insurance_calculator/step_45/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImpl.java
@@ -0,0 +1,21 @@
+package lv.javaguru.travel.insurance.core.underwriting;
+
+import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+class TravelPremiumUnderwritingImpl implements TravelPremiumUnderwriting {
+
+    @Autowired private DateTimeUtil dateTimeUtil;
+
+    @Override
+    public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
+        var daysBetween = dateTimeUtil.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo());
+        return new BigDecimal(daysBetween);
+    }
+
+}
