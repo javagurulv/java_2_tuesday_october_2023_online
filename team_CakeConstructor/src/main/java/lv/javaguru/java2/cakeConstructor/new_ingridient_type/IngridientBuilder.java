59b32fbe50bcbb0a47565ab4bab6868a15344281
IchiuRain <ichiu.rain011@gmail.com>
2023-11-10 12:42:30 +0200

    cake

---
+++ team_CakeConstructor/src/main/java/lv/javaguru/java2/cakeConstructor/new_ingridient_type/IngridientBuilder.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java2.cakeConstructor.new_ingridient_type;
+
+public class IngridientBuilder {
+    private String typeOfIngridient;
+    private String taste;
+
+    public static IngridientBuilder createIngridient(){
+        return new IngridientBuilder();
+    }
+    public Ingridient build(){
+        return new Ingridient(typeOfIngridient,taste);
+    }
+    public IngridientBuilder withType(String type){
+        this.typeOfIngridient=type;
+        return this;
+    }
+
+    public IngridientBuilder withTaste(String taste){
+        this.taste=taste;
+        return this;
+    }
+}
