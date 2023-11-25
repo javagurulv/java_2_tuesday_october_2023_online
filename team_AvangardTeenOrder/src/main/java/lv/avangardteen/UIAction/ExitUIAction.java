9d4a1192864832036d087f6844b517e7944c79dd
Оксана Tarasova <oksana_sineok@mail.ru>
2023-11-25 17:24:56 +0200

    spring_step_2_lesson7

--- team_AvangardTeenOrder/src/main/java/lv/avangardteen/UIAction/ExitUIAction.java
+++ team_AvangardTeenOrder/src/main/java/lv/avangardteen/UIAction/ExitUIAction.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class ExitUIAction implements UIAction {
     @Override
     public void execute() {
-        System.out.println("Good by!");
+        System.out.println("Good bye!");
         System.exit(0);
     }
 }
