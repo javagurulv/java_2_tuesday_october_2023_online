5c51eff5fa804ed47682ba705b5a22a0e18d2393
DESKTOP-6OUGHTK\DELL <tatjana.grigorcaka@gmail.com>
2023-11-27 17:26:06 +0200

    travel_insurance_calculator_step_52_selected_risks_validator_placeholder

--- student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/validations/ValidationErrorFactory.java
+++ student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/validations/ValidationErrorFactory.java
@@ -1,9 +1,12 @@
 package lv.javaguru.travel.insurance.core.validations;
 
 import lv.javaguru.travel.insurance.core.util.ErrorCodeUtil;
+import lv.javaguru.travel.insurance.core.util.Placeholder;
 import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 class ValidationErrorFactory {
@@ -12,6 +15,11 @@ class ValidationErrorFactory {
 
     ValidationError buildError(String errorCode) {
         String errorDescription = errorCodeUtil.getErrorDescription(errorCode);
+        return new ValidationError(errorCode, errorDescription);
+    }
+
+    ValidationError buildError(String errorCode, List<Placeholder> placeholders) {
+        String errorDescription = errorCodeUtil.getErrorDescription(errorCode, placeholders);
         return new ValidationError(errorCode, errorDescription);
     }
 
