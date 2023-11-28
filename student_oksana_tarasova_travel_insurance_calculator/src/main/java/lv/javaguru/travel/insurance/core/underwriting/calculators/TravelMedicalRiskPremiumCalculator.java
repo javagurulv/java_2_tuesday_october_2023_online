5d05adb288f13c0809f372884e6eed38c97f19cb
Оксана Tarasova <oksana_sineok@mail.ru>
2023-11-28 13:08:50 +0200

    insurance_task64_query_default_day_rate

--- student_oksana_tarasova_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelMedicalRiskPremiumCalculator.java
+++ student_oksana_tarasova_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelMedicalRiskPremiumCalculator.java
@@ -1,6 +1,8 @@
 package lv.javaguru.travel.insurance.core.underwriting.calculators;
 
 
+import lv.javaguru.travel.insurance.core.domain.CountryDefaultDayRate;
+import lv.javaguru.travel.insurance.core.repositories.CountryDefaultDayRateRepository;
 import lv.javaguru.travel.insurance.core.underwriting.TravelRiskPremiumCalculator;
 import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
@@ -8,18 +10,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Component
 public class TravelMedicalRiskPremiumCalculator implements TravelRiskPremiumCalculator {
 
     @Autowired
     private DateTimeUtil dateTimeUtil;
+
+    @Autowired private CountryDefaultDayRateRepository countryDefaultDayRateRepository;
+
     @Override
     public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
+        var daysCount = calculateDayCount(request);
+        var countryDefaultRate = findCountryDefaultDayRate(request);
+        return countryDefaultRate.multiply(daysCount)
+                .setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateDayCount(TravelCalculatePremiumRequest request) {
         var daysBetween = dateTimeUtil.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo());
         return new BigDecimal(daysBetween);
     }
+
+    private BigDecimal findCountryDefaultDayRate(TravelCalculatePremiumRequest request) {
+        return countryDefaultDayRateRepository.findByCountryIc(request.getCountry())
+                .map(CountryDefaultDayRate::getDefaultDayRate)
+                .orElseThrow(() -> new RuntimeException("Country day rate not found by country id = " + request.getCountry()));
+    }
+
     @Override
     public String getRiskIc() {
         return "TRAVEL_MEDICAL";
     }
+
 }
