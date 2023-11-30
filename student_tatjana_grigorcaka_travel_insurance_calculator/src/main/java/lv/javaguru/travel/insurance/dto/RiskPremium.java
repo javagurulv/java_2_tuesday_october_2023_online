43b66e909780b96942efad92ec542b62f836b89d
DESKTOP-6OUGHTK\DELL <tatjana.grigorcaka@gmail.com>
2023-11-30 17:55:40 +0200

    travel_insurance_calculator_step_65_bigDecimalSerializer

--- student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/dto/RiskPremium.java
+++ student_tatjana_grigorcaka_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/dto/RiskPremium.java
@@ -1,9 +1,11 @@
 package lv.javaguru.travel.insurance.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lv.javaguru.travel.insurance.core.util.BigDecimalSerializer;
 
 import java.math.BigDecimal;
 
@@ -15,6 +17,8 @@ public class RiskPremium {
 
     private String riskIc;
 
+    @JsonSerialize(using = BigDecimalSerializer.class)
     private BigDecimal premium;
 
 }
+
