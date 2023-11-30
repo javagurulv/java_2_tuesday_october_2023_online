45af4c3c33a55e9080273d97d8e9d37d3693aae6
Оксана Tarasova <oksana_sineok@mail.ru>
2023-11-30 09:03:37 +0200

    insurance_task70_age_domain_repository

--- student_oksana_tarasova_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelMedicalRiskPremiumCalculator.java
+++ student_oksana_tarasova_travel_insurance_calculator/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/TravelMedicalRiskPremiumCalculator.java
@@ -1,7 +1,9 @@
 package lv.javaguru.travel.insurance.core.underwriting.calculators;
 
 
+import lv.javaguru.travel.insurance.core.domain.AgeCoefficient;
 import lv.javaguru.travel.insurance.core.domain.CountryDefaultDayRate;
+import lv.javaguru.travel.insurance.core.repositories.AgeCoefficientRepository;
 import lv.javaguru.travel.insurance.core.repositories.CountryDefaultDayRateRepository;
 import lv.javaguru.travel.insurance.core.underwriting.TravelRiskPremiumCalculator;
 import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
@@ -11,20 +13,25 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Component
 public class TravelMedicalRiskPremiumCalculator implements TravelRiskPremiumCalculator {
-
-    @Autowired
-    private DateTimeUtil dateTimeUtil;
-
+    @Autowired private DateTimeUtil dateTimeUtil;
     @Autowired private CountryDefaultDayRateRepository countryDefaultDayRateRepository;
+    @Autowired private AgeCoefficientRepository ageCoefficientRepository;
 
     @Override
     public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
         var daysCount = calculateDayCount(request);
         var countryDefaultRate = findCountryDefaultDayRate(request);
-        return countryDefaultRate.multiply(daysCount)
+        var ageCoefficient = findAgeCoefficient(request);
+        return countryDefaultRate
+                .multiply(daysCount)
+                .multiply(ageCoefficient)
                 .setScale(2, RoundingMode.HALF_UP);
     }
 
@@ -37,6 +44,25 @@ public class TravelMedicalRiskPremiumCalculator implements TravelRiskPremiumCalc
         return countryDefaultDayRateRepository.findByCountryIc(request.getCountry())
                 .map(CountryDefaultDayRate::getDefaultDayRate)
                 .orElseThrow(() -> new RuntimeException("Country day rate not found by country id = " + request.getCountry()));
+    }
+
+    private BigDecimal findAgeCoefficient(TravelCalculatePremiumRequest request) {
+        int age = calculateAge(request);
+        return ageCoefficientRepository.findCoefficient(age)
+                .map(AgeCoefficient::getCoefficient)
+                .orElseThrow(() -> new RuntimeException("Age coefficient not found for age = " + age));
+    }
+
+    private Integer calculateAge(TravelCalculatePremiumRequest request) {
+        LocalDate personBirthDate = toLocalDate(request.getPersonBirthDate());
+        LocalDate currentDate = toLocalDate(dateTimeUtil.getCurrentDateTime());
+        return Period.between(personBirthDate, currentDate).getYears();
+    }
+
+    private LocalDate toLocalDate(Date date) {
+        return date.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
     }
 
     @Override
