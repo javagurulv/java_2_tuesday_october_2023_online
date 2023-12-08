64ea224f609f269f6733014806a2f9e3a2d99ce1
AndrejsCekalins <batarejka2006@gmail.com>
2023-12-08 11:18:21 +0200

    student_andrejs_cekalins_travel_insurance_calculate_step_75_CountryValidationTest

---
+++ student_andrejs_cekalins_travel_insurance_calculator/src/test/java/lv/javaguru/travel/insurance/core/validations/CountryValidationTest.java
@@ -0,0 +1,92 @@
+package lv.javaguru.travel.insurance.core.validations;
+
+import lv.javaguru.travel.insurance.core.domain.ClassifierValue;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import lv.javaguru.travel.insurance.repositories.ClassifierValueRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CountryValidationTest {
+
+    @Mock
+    private ClassifierValueRepository classifierValueRepository;
+
+    @Mock
+    private ValidationErrorFactory errorFactory;
+
+    @InjectMocks
+    private CountryValidation validation;
+
+    @Test
+    public void shouldNotReturnErrorWhenNotContainTravelMedicalRisk() {
+        TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        when(request.getSelectedRisks()).thenReturn(List.of("TRAVEL_EVACUATION"));
+        Optional<ValidationError> validationErrorOpt = validation.validate(request);
+        assertTrue(validationErrorOpt.isEmpty());
+        verifyNoInteractions(classifierValueRepository, errorFactory);
+    }
+    @Test
+    public void shouldReturnNoErrorWhenSelectedRisksIsNull() {
+        TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        when(request.getSelectedRisks()).thenReturn(null);
+        Optional<ValidationError> errorOpt = validation.validate(request);
+        assertTrue(errorOpt.isEmpty());
+    }
+
+    @Test
+    public void shouldNotReturnErrorWhenContainTravelMedicalRiskAndCountryIsBlank() {
+        TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        when(request.getSelectedRisks()).thenReturn(List.of("TRAVEL_MEDICAL"));
+        when(request.getCountry()).thenReturn("");
+        Optional<ValidationError> validationErrorOpt = validation.validate(request);
+        assertTrue(validationErrorOpt.isEmpty());
+        verifyNoInteractions(classifierValueRepository, errorFactory);
+    }
+    @Test
+    public void shouldNotReturnErrorWhenContainTravelMedicalRiskAndCountryIsNull() {
+        TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        when(request.getSelectedRisks()).thenReturn(List.of("TRAVEL_MEDICAL"));
+        when(request.getCountry()).thenReturn(null);
+        Optional<ValidationError> validationErrorOpt = validation.validate(request);
+        assertTrue(validationErrorOpt.isEmpty());
+        verifyNoInteractions(classifierValueRepository, errorFactory);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenCountryNotExistInDb() {
+        TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        when(request.getSelectedRisks()).thenReturn(List.of("TRAVEL_MEDICAL"));
+        when(request.getCountry()).thenReturn("LATVIA");
+        when(classifierValueRepository.findByClassifierTitleAndIc("COUNTRY", "LATVIA"))
+                .thenReturn(Optional.empty());
+        ValidationError validationError = mock(ValidationError.class);
+        when(errorFactory.buildError("ERROR_CODE_15")).thenReturn(validationError);
+        Optional<ValidationError> validationErrorOpt = validation.validate(request);
+        assertTrue(validationErrorOpt.isPresent());
+        assertSame(validationError, validationErrorOpt.get());
+    }
+
+    @Test
+    public void shouldNotReturnErrorWhenCountryExistInDb() {
+        TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
+        when(request.getSelectedRisks()).thenReturn(List.of("TRAVEL_MEDICAL"));
+        when(request.getCountry()).thenReturn("LATVIA");
+        ClassifierValue classifierValue = mock(ClassifierValue.class);
+        when(classifierValueRepository.findByClassifierTitleAndIc("COUNTRY", "LATVIA"))
+                .thenReturn(Optional.of(classifierValue));
+        Optional<ValidationError> validationErrorOpt = validation.validate(request);
+        assertTrue(validationErrorOpt.isEmpty());
+        verifyNoInteractions(errorFactory);
+    }
+}
