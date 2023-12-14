6deeae0bc5efa546cc20b65cd4995527af896c1e
AndrejsCekalins <batarejka2006@gmail.com>
2023-12-14 15:31:37 +0200

    fitness_club_dbDataInsert

--- team_fitness_club/database/dbDataInsertScript.sql
+++ team_fitness_club/database/dbDataInsertScript.sql
@@ -1,54 +1,60 @@
 INSERT INTO clients(first_name, last_name, personal_code)
-VALUES ('firstnameA', 'lastnameA', 11111);
+VALUES ('Vasja', 'Pupkin', 11111);
 
-INSERT INTO clients(id, first_name, last_name, personal_code)
-VALUES (102, 'firstnameB', 'lastnameB', 22222);
+INSERT INTO clients(first_name, last_name, personal_code)
+VALUES ('Andrey', 'Petrov', 22222);
 
-INSERT INTO clients(id, first_name, last_name, personal_code)
-VALUES (103, 'firstnameC', 'lastnameC', 33333);
+INSERT INTO clients(first_name, last_name, personal_code)
+VALUES ('Dmitrij', 'Ivanov', 33333);
 
-INSERT INTO clients(id, first_name, last_name, personal_code)
-VALUES (103, 'firstnameD', 'lastnameD', 33A22);
+INSERT INTO clients(first_name, last_name, personal_code)
+VALUES ('Viktor', 'Sidorov', 33A22);
 
-INSERT INTO age_groups(age_group)
-VALUES ('CHILD');
 
-INSERT INTO age_groups(id, age_group)
+INSERT INTO age_groups
+VALUES (1, 'CHILD');
+
+INSERT INTO age_groups
 VALUES (2, 'ADULT');
 
-INSERT INTO age_groups(id, age_group)
+INSERT INTO age_groups
 VALUES (3, 'SENIOR');
 
 
-INSERT INTO workouts(workout)
-VALUES ('GYM');
+INSERT INTO workouts
+VALUES (1, 'GYM');
 
-INSERT INTO workouts(id, workout)
+INSERT INTO workouts
 VALUES (2, 'SWIMMING_POOL');
 
-INSERT INTO workouts(id, workout)
+INSERT INTO workouts
 VALUES (3, 'GROUP_CLASSES');
 
 
-insert into fitness_centres(fitness_centre)
-values ('IMANTA');
+INSERT INTO fitness_centres
+VALUES (1, 'IMANTA');
 
-insert into fitness_centres(id, fitness_centre)
-values (101, 'AKROPOLE');
+INSERT INTO fitness_centres
+VALUES (2,'AKROPOLE');
 
-insert into fitness_centres(id, fitness_centre)
-values (102, 'SAGA');
+INSERT INTO fitness_centres
+VALUES (3, 'SAGA');
 
-insert into fitness_centres(id, fitness_centre)
-values (103, 'RIGA_PLAZA');
+INSERT INTO fitness_centres
+VALUES (4, 'RIGA_PLAZA');
 
-insert into fitness_centres(id, fitness_centre)
-values (104, 'ZOLITUDE');
+INSERT INTO fitness_centres
+VALUES (5, 'ZOLITUDE');
 
 
-insert into member_card(client_id, age_group_id, workout_id, fitness_centre_id, term_of_contract)
-values (102, 2, 2, 101, '2024-10-10 23:59:59');
+INSERT INTO member_card(client_id, age_group_id, workout_id, fitness_centre_id, term_of_contract)
+VALUES (1, 2, 2, 1, '2024-03-31 23:59:59');
 
-insert into member_card(client_id, age_group_id, workout_id, fitness_centre_id, term_of_contract)
-values (103, 3, 3, 104, '2024-09-10 23:59:59');
+INSERT INTO member_card(client_id, age_group_id, workout_id, fitness_centre_id, term_of_contract)
+VALUES (2, 3, 3, 4, '2024-04-30 23:59:59');
 
+INSERT INTO member_card(client_id, age_group_id, workout_id, fitness_centre_id, term_of_contract)
+VALUES (3, 1, 1, 5, '2024-03-31 23:59:59');
+
+INSERT INTO member_card(client_id, age_group_id, workout_id, fitness_centre_id, term_of_contract)
+VALUES (4, 2, 1, 3, '2024-05-31 23:59:59');
