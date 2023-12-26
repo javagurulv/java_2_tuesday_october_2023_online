42b54fa949ac8321390bd3edd3c634c131d61934
DESKTOP-6OUGHTK\DELL <tatjana.grigorcaka@gmail.com>
2023-12-26 17:15:46 +0200

    travel_insurance_calculator_step_94_version2

---
+++ student_tatjana_grigorcaka_travel_insurance_calculator_v2/src/test/java/lv/javaguru/travel/insurance/core/util/DateTimeUtilTest.java
@@ -0,0 +1,44 @@
+package lv.javaguru.travel.insurance.core.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DateTimeUtilTest {
+
+    private DateTimeUtil dateTimeUtil = new DateTimeUtil();
+    @Test
+    public void testDaysBetweenZero(){
+        Date date1 = createDate("01.01.2024");
+        Date date2 = createDate("01.01.2024");
+        var daysBetween = dateTimeUtil.getDaysBetween(date1, date2);
+        assertEquals(daysBetween, 0L);
+    }
+    @Test
+    public void testDaysBetweenPositive(){
+        Date date1 = createDate("01.01.2024");
+        Date date2 = createDate("15.01.2024");
+        var daysBetween = dateTimeUtil.getDaysBetween(date1, date2);
+        assertEquals(daysBetween, 14L);
+    }
+    @Test
+    public void testDaysBetweenNegative(){
+        Date date1 = createDate("01.01.2024");
+        Date date2 = createDate("25.12.2023");
+        var daysBetween = dateTimeUtil.getDaysBetween(date1, date2);
+        assertEquals(daysBetween, -7L);
+    }
+
+    private Date createDate(String dateStr) {
+        try {
+            return new SimpleDateFormat("dd.MM.yyyy").parse(dateStr);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
