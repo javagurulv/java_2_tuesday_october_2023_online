cb4ffa3e42db8f97887d1bee21b188e0b562a876
AndrejsCekalins <batarejka2006@gmail.com>
2023-12-26 13:18:16 +0200

    student_andrejs_cekalins_travel_insurance_calculate_step_98_TravelAgreementValidatorImpl_refactoring

---
+++ student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelAgreementFieldValidator.java
@@ -0,0 +1,49 @@
+package lv.javaguru.travel.insurance.core.validations;
+
+import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import lv.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class TravelAgreementFieldValidator {
+    @Autowired
+    private List<TravelAgreementFieldValidation> agreementFieldValidations;
+
+
+    public List<ValidationErrorDTO> validate(AgreementDTO request) {
+        List<ValidationErrorDTO> singleErrors = collectSingleAgreementErrors(request);
+        List<ValidationErrorDTO> listErrors = collectListAgreementErrors(request);
+        return concatenateLists(singleErrors, listErrors);
+    }
+
+    private List<ValidationErrorDTO> collectSingleAgreementErrors(AgreementDTO request) {
+        return agreementFieldValidations.stream()
+                .map(validation -> validation.validate(request))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private List<ValidationErrorDTO> collectListAgreementErrors(AgreementDTO request) {
+        return agreementFieldValidations.stream()
+                .map(validation -> validation.validateList(request))
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+    private List<ValidationErrorDTO> concatenateLists(List<ValidationErrorDTO> singleErrors,
+                                                      List<ValidationErrorDTO> listErrors) {
+        return Stream.concat(singleErrors.stream(), listErrors.stream())
+                .collect(Collectors.toList());
+    }
+
+}
