c2b3c8ad762b96ecac41b1dbedf6a9f9e4bab325
AndrejsCekalins <batarejka2006@gmail.com>
2023-12-26 14:47:38 +0200

    student_andrejs_cekalins_travel_insurance_calculate_step_99_TravelRiskPremiumCalculator_v2

---
+++ student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/medical/RiskLimitLevelCalculator.java
@@ -0,0 +1,38 @@
+package lv.javaguru.travel.insurance.core.underwriting.calculators.medical;
+
+
+import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import lv.javaguru.travel.insurance.core.domain.MedicalRiskLimitLevel;
+import lv.javaguru.travel.insurance.core.repositories.MedicalRiskLimitLevelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class RiskLimitLevelCalculator {
+
+    @Value("${medical.risk.age.coefficient.enabled:false}")
+    private Boolean medicalRiskLimitLevelEnabled;
+
+    @Autowired
+    private MedicalRiskLimitLevelRepository riskLimitLevelRepository;
+
+    BigDecimal calculate(AgreementDTO agreement) {
+        return medicalRiskLimitLevelEnabled
+                ? getCoefficient(agreement)
+                : getDefaultValue();
+    }
+
+    private BigDecimal getCoefficient(AgreementDTO agreement) {
+        return riskLimitLevelRepository.findByMedicalRiskLimitLevelIc(agreement.getMedicalRiskLimitLevel())
+                .map(MedicalRiskLimitLevel::getCoefficient)
+                .orElseThrow(() -> new RuntimeException("Medical risk limit level not found by = " + agreement.getMedicalRiskLimitLevel()));
+    }
+
+    private BigDecimal getDefaultValue() {
+        return BigDecimal.ONE;
+    }
+
+}
