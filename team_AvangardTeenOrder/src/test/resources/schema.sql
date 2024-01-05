cc5a0f11973a9dbc5622a9529137b922b95355b7
Оксана Tarasova <oksana_sineok@mail.ru>
2024-01-05 13:39:45 +0200

    db_creation_insert

--- team_AvangardTeenOrder/src/test/resources/schema.sql
+++ team_AvangardTeenOrder/src/test/resources/schema.sql
@@ -1,59 +1,54 @@
 DROP TABLE IF EXISTS clients CASCADE;
-DROP TABLE IF EXISTS client_size CASCADE;
+DROP TABLE IF EXISTS parameters CASCADE;
 DROP TABLE IF EXISTS wheelchair CASCADE;
 DROP TABLE IF EXISTS order_components CASCADE;
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=1;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=1;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `avangard_teen_order` DEFAULT CHARACTER SET utf8 ;
+USE `avangard_teen_order` ;
 
 CREATE TABLE IF NOT EXISTS  `clients` (
 `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
 `name_surname` VARCHAR (200) NOT NULL,
 `personal_code` BIGINT NOT NULL,
 `phone` BIGINT NOT NULL,
-`address` VARCHAR (300) NOT NULL,
-`order_id` BIGINT
+`address` VARCHAR (300) NOT NULL
 
 )
 ENGINE = InnoDB
 AUTO_INCREMENT = 10;
 
-ALTER TABLE `clients`
-add foreign key (order_id) REFERENCES wheelchair (id) ON DELETE CASCADE;
-
-CREATE UNIQUE INDEX ix_clients_wheelchair_order ON clients(id, order_id);
 CREATE UNIQUE INDEX clients_type_index ON clients(name_surname, personal_code);
 
-CREATE TABLE IF NOT EXISTS  `client_size` (
+CREATE TABLE IF NOT EXISTS  `parameters` (
 `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `client_id` BIGINT NOT NULL,
 `pelvisWidth` INT NOT NULL,
 `thighLength` INT NOT NULL,
 `backHeight` INT NOT NULL,
-`shinLength` INT NOT NULL,
-`order_id` BIGINT
+`shinLength` INT NOT NULL
+
 )
 ENGINE = InnoDB
 AUTO_INCREMENT = 10;
 
-ALTER TABLE `client_size`
-add foreign key (order_id) REFERENCES wheelchair (id)  ON DELETE CASCADE;
-
-CREATE UNIQUE INDEX client_size_type_index ON client_size(order_id);
-
 CREATE TABLE IF NOT EXISTS  `wheelchair` (
-`id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`id` BIGINT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+`client_id` BIGINT NOT NULL,
 `seatWidth` INT NOT NULL,
 `seatDepth` INT NOT NULL,
 `footrestLength` INT NOT NULL,
 `bachHeight` INT NOT NULL,
-`price` DOUBLE DEFAULT 177000.0,
+`price` DOUBLE DEFAULT 177000.0
 )
 ENGINE = InnoDB
 AUTO_INCREMENT = 10;
 
 ALTER TABLE `wheelchair`
 add foreign key (client_id) REFERENCES clients (id)  ON DELETE CASCADE;
-
-CREATE UNIQUE INDEX wheelchair_type_index ON wheelchair(client_id);
-
 
 CREATE TABLE IF NOT EXISTS  `order_components` (
 `id` BIGINT NOT NULL PRIMARY KEY auto_increment,
@@ -70,24 +65,12 @@ add foreign key (component_id) REFERENCES `components` (id);
 ALTER TABLE `order_components`
 add foreign key (wheelchair_id) REFERENCES `wheelchair` (id)  ON DELETE CASCADE;
 
-CREATE UNIQUE INDEX order_components_components_index ON order_components(component_id);
 CREATE UNIQUE INDEX order_components_wheelchair_index ON order_components(wheelchair_id);
 
 
-CREATE TABLE IF NOT EXISTS `category`(
-`id` BIGINT NOT NULL AUTO_INCREMENT,
-`title` VARCHAR(20) NOT NULL,
-PRIMARY KEY (`id`)
-)
-
-ENGINE = InnoDB
-AUTO_INCREMENT = 1;
-
-CREATE UNIQUE INDEX category_type_index ON category(title);
-
 CREATE TABLE IF NOT EXISTS `components` (
 `id` INT NOT NULL AUTO_INCREMENT,
-`category_key` BIGINT NOT NULL,
+`category` VARCHAR(20) NOT NULL,
 `marking` VARCHAR (20) NOT NULL UNIQUE,
 `information` VARCHAR (2000) NOT NULL,
 `price` DOUBLE,
@@ -97,8 +80,7 @@ PRIMARY KEY (`id`)
 ENGINE = InnoDB
 AUTO_INCREMENT = 1;
 
-ALTER TABLE `components`
-ADD FOREIGN KEY (`category_key`)
-REFERENCES `category` (`id`);
 
-CREATE UNIQUE INDEX category_key_index ON components(category_key);
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
