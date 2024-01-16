4eb03bb0970abcca7e90605895e25596f61cba20
AndrejsCekalins <batarejka2006@gmail.com>
2024-01-16 22:36:47 +0200

    fitness_club_

--- team_fitness_club/src/main/java/fitness_club/core/services/vlidators/ageGroup/SetAgeGroupToClientRequestValidator.java
+++ team_fitness_club/src/main/java/fitness_club/core/services/vlidators/ageGroup/SetAgeGroupToClientRequestValidator.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-@Component
+//@Component
 public class SetAgeGroupToClientRequestValidator {
 
     @Autowired
