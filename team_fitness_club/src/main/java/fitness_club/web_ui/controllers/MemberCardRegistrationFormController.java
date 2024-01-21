e5f9f2bd4577435e15c2a2faa07d6ddfb60cb23b
Anetobiz <anetobiz@gmail.com>
2024-01-21 19:00:00 +0200

    dropdowns for member card filling form

--- team_fitness_club/src/main/java/fitness_club/web_ui/controllers/MemberCardRegistrationFormController.java
+++ team_fitness_club/src/main/java/fitness_club/web_ui/controllers/MemberCardRegistrationFormController.java
@@ -1,5 +1,11 @@
 package fitness_club.web_ui.controllers;
 
+import fitness_club.core.database.jpa.JpaAgeGroupRepository;
+import fitness_club.core.database.jpa.JpaFitnessCentersRepository;
+import fitness_club.core.database.jpa.JpaWorkoutsRepository;
+import fitness_club.core.domain.AgeGroup;
+import fitness_club.core.domain.FitnessCenter;
+import fitness_club.core.domain.Workout;
 import fitness_club.core.requests.*;
 import fitness_club.core.responses.*;
 import fitness_club.core.services.*;
@@ -8,25 +14,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class MemberCardRegistrationFormController {
 
     @Autowired
     private MemberCardRegistrationFormService memberCardRegistrationService;
-
+    @Autowired
+    private JpaWorkoutsRepository workoutsRepository;
+    @Autowired
+    private JpaAgeGroupRepository ageGroupRepository;
+    @Autowired
+    private JpaFitnessCentersRepository fitnessCentersRepository;
     @Autowired
     private SearchClientService searchClientService;
     @Autowired
     private SearchAgeGroupService searchAgeGroupService;
-
     @Autowired
     private SearchFitnessCenterService searchFitnessCenterService;
-
     @Autowired
     private SearchWorkoutService searchWorkoutService;
 
     @GetMapping(value = "/memberCardRegistrationForm")
     public String showMemberCardFormFilling(ModelMap modelMap) {
+
+        List<AgeGroup> ageGroups = ageGroupRepository.findAll();
+        modelMap.addAttribute("ageGroups", ageGroups);
+
+        List<Workout> workouts = workoutsRepository.findAll();
+        modelMap.addAttribute("workouts", workouts);
+
+        List<FitnessCenter> fitnessCenters = fitnessCentersRepository.findAll();
+        modelMap.addAttribute("fitnessCenters", fitnessCenters);
+
         modelMap.addAttribute("request", new MemberCardRegistrationFormRequest());
         modelMap.addAttribute("searchRequest", new SearchMemberCardRequest());
         return "memberCardRegistrationForm";
