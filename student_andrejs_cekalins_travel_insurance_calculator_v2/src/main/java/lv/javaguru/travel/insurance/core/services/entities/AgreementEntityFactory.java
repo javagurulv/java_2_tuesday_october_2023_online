50c1bcd9da2cf0f093aaf1482128df10e20369ef
AndrejsCekalins <batarejka2006@gmail.com>
2024-02-08 19:30:03 +0200

    student_andrejs_cekalins_travel_insurance_calculate_step_125_uuid_column_creation_in_db

--- student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/core/services/entities/AgreementEntityFactory.java
+++ student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/core/services/entities/AgreementEntityFactory.java
@@ -10,6 +10,8 @@ import lv.javaguru.travel.insurance.core.repositories.entities.SelectedRiskEntit
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class AgreementEntityFactory {
     @Autowired
@@ -40,6 +42,7 @@ public class AgreementEntityFactory {
 
     private AgreementEntity saveAgreement(AgreementDTO agreementDTO) {
         AgreementEntity agreementEntity = new AgreementEntity();
+       // agreementEntity.setUuid(UUID.randomUUID().toString());
         agreementEntity.setAgreementDateFrom(agreementDTO.getAgreementDateFrom());
         agreementEntity.setAgreementDateTo(agreementDTO.getAgreementDateTo());
         agreementEntity.setCountry(agreementDTO.getCountry());
