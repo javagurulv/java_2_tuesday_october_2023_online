229ca146be3d89d5cc0f3d8c15faa3c4debbd1de
AndrejsCekalins <batarejka2006@gmail.com>
2024-02-13 21:52:35 +0200

    student_andrejs_cekalins_travel_insurance_calculator_step_128_getAgreementDtoConverter

--- student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/rest/internal/TravelGetAgreementRestController.java
+++ student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/rest/internal/TravelGetAgreementRestController.java
@@ -2,6 +2,10 @@ package lv.javaguru.travel.insurance.rest.internal;
 
 import com.google.common.base.Stopwatch;
 
+import lv.javaguru.travel.insurance.core.api.comamnd.TravelGetAgreementCoreCommand;
+import lv.javaguru.travel.insurance.core.api.comamnd.TravelGetAgreementCoreResult;
+import lv.javaguru.travel.insurance.core.services.TravelGetAgreementService;
+import lv.javaguru.travel.insurance.dto.internal.GetAgreementDtoConverter;
 import lv.javaguru.travel.insurance.dto.internal.TravelGetAgreementResponse;
 import lv.javaguru.travel.insurance.rest.common.TravelCalculatePremiumRequestExecutionTimeLogger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,33 +17,35 @@ import java.util.Date;
 @RequestMapping("/insurance/travel/api/internal/agreement")
 public class TravelGetAgreementRestController {
 
-    @Autowired private TravelGetAgreementRequestLogger requestLogger;
-    @Autowired private TravelGetAgreementResponseLogger responseLogger;
-    @Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
+    @Autowired
+    private TravelGetAgreementRequestLogger requestLogger;
+    @Autowired
+    private TravelGetAgreementResponseLogger responseLogger;
+    @Autowired
+    private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
+    @Autowired
+    private TravelGetAgreementService getAgreementService;
+    @Autowired
+    private GetAgreementDtoConverter dtoConverter;
 
-   @GetMapping (path = "/{uuid}",
-   produces = "application/json")
+    @GetMapping(path = "/{uuid}",
+            produces = "application/json")
     public TravelGetAgreementResponse getAgreement(@PathVariable String uuid) {
-       Stopwatch stopwatch = Stopwatch.createStarted();
-       TravelGetAgreementResponse response = processRequest(uuid);
-       new TravelGetAgreementResponse();
-       response.setAgreementDateFrom(new Date());
-       response.setAgreementDateTo(new Date());
-       response.setUuid(uuid);
-       executionTimeLogger.logExecutionTime(stopwatch);
-       return response;
-   }
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        TravelGetAgreementResponse response = processRequest(uuid);
+        executionTimeLogger.logExecutionTime(stopwatch);
+        return response;
+    }
 
-   private TravelGetAgreementResponse processRequest(String uuid) {
-       requestLogger.log(uuid);
+    private TravelGetAgreementResponse processRequest(String uuid) {
+        requestLogger.log(uuid);
 
-      TravelGetAgreementResponse response  = new TravelGetAgreementResponse();
-       response.setAgreementDateFrom(new Date());
-       response.setAgreementDateTo(new Date());
-       response.setUuid(uuid);
+        TravelGetAgreementCoreCommand coreCommand = dtoConverter.buildCoreCommand(uuid);
+        TravelGetAgreementCoreResult coreResult= getAgreementService.getAgreement(coreCommand);
+        TravelGetAgreementResponse response = dtoConverter.buildResponse(coreResult);
 
-       responseLogger.log(response);
-       return response;
-   }
+        responseLogger.log(response);
+        return response;
+    }
 
 }
