2ecea163a244416a5d6f72628f5b9b8c77f5ebcc
AndrejsCekalins <batarejka2006@gmail.com>
2024-04-12 12:39:08 +0300

    student_andrejs_cekalins_travel_insurance_calculator_simpleJob_class

---
+++ student_andrejs_cekalins_travel_insurance_calculator_v2/src/main/java/lv/javaguru/travel/insurance/core/jobs/SimpleJob.java
@@ -0,0 +1,20 @@
+package lv.javaguru.travel.insurance.core.jobs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class SimpleJob {
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleJob.class);
+
+    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    public void doJob() {
+        logger.info("SimpleJob stated");
+        logger.info("SimpleJob finished");
+    }
+}
